c53e56cabd3010fe62e4804e722c4cedeb353541
Laurent Van Acker <laurentvanacker@gmail.com>
2021-08-18 00:36:05 +0200

    added menu, pause menu, audio engine

---
+++ src/Menu/menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+onready var _tab_container := $CanvasLayer/TabContainer
+
+func _ready():
+	if ConfigData.skip_menu:
+		if ConfigData.verbose_mode : print("Automatically skipping menu as requested by configuration data...")
+		Flow.go_to_game()
+	else:
+		pass
+#		_tab_container.set_current_tab(class_pause_tab.TABS.MAIN)
