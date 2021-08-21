8902da4a6a7918beb62986d81f024c005e9d2f76
Carto <cryptic_outcast@hotmail.com>
2021-08-20 21:32:11 -0500

    Den structures don't expand beyond barriers

--- src/Structures/BNetStructure/Den/den_structure.gd
+++ src/Structures/BNetStructure/Den/den_structure.gd
@@ -42,7 +42,7 @@ func add_consuming_cell(p_cell: Cell):
 
 func cell_consumed(from: Cell, cell_consumed: Cell):
 	bnet.consumed_cells[cell_consumed.hex_coords] = cell_consumed
-	var next_cell = from.breadth_search_neighbours()
+	var next_cell = self.breadth_search_neighbours(from)
 
 	
 	if next_cell != null:
@@ -75,3 +75,63 @@ func state_to_inept():
 	
 func state_to_producing():
 	emit_signal("no_longer_inept")
+
+
+#############
+## Path finding for next cell
+#############
+
+func has_edge(from: Cell, to: Cell):
+	var direction = to.hex_coords - from.hex_coords
+	return from.has_edge(direction)
+
+func breadth_search_neighbours(from: Cell):
+	# Store neighbours to search
+	var ns_queue = []
+	var visited: Dictionary
+
+	# Start of algorithm
+	ns_queue.append(from)
+
+	# Invariant
+	while ns_queue.empty() == false:
+		# Look at first cell
+		var c = ns_queue.pop_front()
+		# No need to look at it again
+		visited[c] = true
+
+		# Get cell's neighbours
+		var cell_neighbours: Array = c.neighbours
+
+		var neighbour_size = cell_neighbours.size()
+		# Array to remove neighbours when searching for a random neighbour
+		var choosing_array = []
+		# Fill array
+		for n in cell_neighbours:
+			choosing_array.append(n)
+
+		# Allow random neighbour selection
+		randomize()
+		var amount_left = neighbour_size
+		# Go over the neighbours in a random manner
+		while amount_left > 0:
+			var random_index = randi() % amount_left
+			var cell_rand = choosing_array[random_index]
+
+			if has_edge(c, cell_rand) == false:
+			# Found neighbour to acquire next
+				if cell_rand.state_machine.current_state.name == "Available":
+					return cell_rand
+				elif cell_rand.state_machine.current_state.name == "BNet" and cell_rand.bunnies.reached_max_capacity() == false:
+					return cell_rand
+				else:
+					# Investigate later, if not already visited
+					if visited.has(cell_rand) == false and cell_rand.state_machine.current_state.name != "Water":
+						ns_queue.append(cell_rand)
+				# remove random cell. No need to look at it again.
+				# Avoids getting the same cell twice.
+				choosing_array.erase(cell_rand)
+
+			amount_left -= 1
+
+	return null
